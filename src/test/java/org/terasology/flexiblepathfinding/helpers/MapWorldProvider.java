367eebe71f11ace011a7e7de6e80b7a095032221
jdrueckert <jd.rueckert@googlemail.com>
2020-12-26 20:42:42 +0100

    feat(JOML): migrate world generation (#13)

--- src/test/java/org/terasology/flexiblepathfinding/helpers/MapWorldProvider.java
+++ src/test/java/org/terasology/flexiblepathfinding/helpers/MapWorldProvider.java
@@ -27,6 +27,7 @@ import org.terasology.math.geom.Vector3i;
 import org.terasology.world.WorldChangeListener;
 import org.terasology.world.WorldProvider;
 import org.terasology.world.block.Block;
+import org.terasology.world.block.BlockRegion;
 import org.terasology.world.block.BlockUri;
 import org.terasology.world.internal.ChunkViewCore;
 import org.terasology.world.internal.WorldInfo;
@@ -329,7 +330,7 @@ public class MapWorldProvider implements WorldProvider {
     }
 
     @Override
-    public Collection<Region3i> getRelevantRegions() {
+    public Collection<BlockRegion> getRelevantRegions() {
         return null;
     }
 
